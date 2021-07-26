b62126a7667ad13294c37fb1aff20a8b618ccde6
David Enoragbon <david.enoragbon@decagon.dev>
2021-07-27 00:30:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import Home from "./components/Home";
+// import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className='App'>
+        <Route path='/' exact>
+          {/* <Home /> */}
+        </Route>
+        <Route path='/cart'>{/* <Cart /> */}</Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
